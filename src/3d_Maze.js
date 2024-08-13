d7976b3bbfadabb3ccaef697a734290459470a46
RexAu0316 <160446866+RexAu0316@users.noreply.github.com>
2024-08-13 17:06:56 +0800

    Update 3d_Maze.js

--- src/3d_Maze.js
+++ src/3d_Maze.js
@@ -50,7 +50,7 @@ window.initGame = (React, assetsUrl) => {
 
     return React.createElement('mesh', {
       ref: playerRef,
-      position: [9.5, 0.5, -9.5], // Centered position
+      position: [9.5, 0.5, -8.5], // Centered position
       geometry: new THREE.BoxGeometry(0.5, 1, 0.5),
       material: new THREE.MeshStandardMaterial({ color: 'blue' })
     });
