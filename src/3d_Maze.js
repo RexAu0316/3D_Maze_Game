3177a6a6d3b4944a37b2fd6e1b28b04248225efa
RexAu0316 <160446866+RexAu0316@users.noreply.github.com>
2024-08-13 11:21:32 +0800

    Update 3d_Maze.js

--- src/3d_Maze.js
+++ src/3d_Maze.js
@@ -1,4 +1,4 @@
-window.initSimpleMaze = (React, assetsUrl) => {
+window.initSimpleMazeGame = (React, assetsUrl) => {
   const { useRef } = React;
   const { useFrame } = window.ReactThreeFiber;
   const THREE = window.THREE;
@@ -14,7 +14,6 @@ window.initSimpleMaze = (React, assetsUrl) => {
 
   function Maze() {
     return React.createElement(React.Fragment, null,
-      // Define the maze layout using walls
       React.createElement(Wall, { position: [0, 0, 0], scale: [1, 1, 10] }), // Center wall
       React.createElement(Wall, { position: [-5, 0, -2], scale: [10, 1, 0.5] }), // Horizontal wall
       React.createElement(Wall, { position: [-3, 0, 2], scale: [3, 1, 0.5] }), // Vertical wall
@@ -29,7 +28,26 @@ window.initSimpleMaze = (React, assetsUrl) => {
     );
   }
 
-  return Maze;
+  function MazeGame() {
+    const cameraRef = useRef();
+
+    // Set the camera position to get an overview of the maze
+    useFrame(() => {
+      if (cameraRef.current) {
+        cameraRef.current.position.set(0, 5, 10); // Change Y to 5 and Z to 10 for an overview
+        cameraRef.current.lookAt(0, 0, 0); // Look at the center of the maze
+      }
+    });
+
+    return React.createElement(React.Fragment, null,
+      React.createElement('ambientLight', { intensity: 0.5 }),
+      React.createElement('pointLight', { position: [10, 10, 10] }),
+      React.createElement('perspectiveCamera', { ref: cameraRef }),
+      React.createElement(Maze)
+    );
+  }
+
+  return MazeGame;
 };
 
-console.log('3D Simple Maze structure loaded');
+console.log('3D Simple Maze game structure loaded');
