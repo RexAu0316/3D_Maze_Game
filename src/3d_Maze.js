3d6cb86ff79afee9e00c583d1965c100420edca6
RexAu0316 <160446866+RexAu0316@users.noreply.github.com>
2024-08-16 14:01:03 +0800

    Update 3d_Maze.js

--- src/3d_Maze.js
+++ src/3d_Maze.js
@@ -65,7 +65,7 @@ window.initGame = (React, assetsUrl) => {
       }
     });
 
-    return React.createElement('mesh', { ref: playerRef, position: [0, 0.5, 0] },
+    return React.createElement('mesh', { ref: playerRef, position: [8.5, 0.5, -8.5] },
       React.createElement('boxGeometry', { args: [1, 1, 1] }),
       React.createElement('meshStandardMaterial', { color: 'blue' })
     );
