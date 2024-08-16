9244e0bd9ee824f301ea15dc8a7ea043fea359e8
RexAu0316 <160446866+RexAu0316@users.noreply.github.com>
2024-08-16 16:42:53 +0800

    Update 3d_Maze.js

--- src/3d_Maze.js
+++ src/3d_Maze.js
@@ -69,7 +69,7 @@ const checkCollision = (nextPosition) => {
       }
     });
 
-    return React.createElement('mesh', { ref: playerRef, position: [-5, 0.5, -5] },
+    return React.createElement('mesh', { ref: playerRef, position: [-10, 0.5, -10] },
       React.createElement('boxGeometry', { args: [0.5, 1, 0.5] }),
       React.createElement('meshStandardMaterial', { color: 'blue' })
     );
